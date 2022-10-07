47f744c572b66624498e81cdd654816a1fe91d12
Rihab Id <rihab.idoudi@esprit.tn>
2022-10-07 13:38:56 +0200

    msg 3

--- src/main/java/tn/esponline/entities/Competence.java
+++ src/main/java/tn/esponline/entities/Competence.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 public class Competence implements Serializable {
 	@Id
 	private int id;
-	
+	//commentaire different
 	private String adresse;
 
 	public Competence() {
